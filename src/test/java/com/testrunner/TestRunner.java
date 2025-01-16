2ccb29d91588a67f5373af0ac2b71cdcae3b18c0
abhij <abhij@Abhijit.lan>
2025-01-16 12:36:10 +0530

    test

--- src/test/java/com/testrunner/TestRunner.java
+++ src/test/java/com/testrunner/TestRunner.java
@@ -3,15 +3,17 @@ package com.testrunner;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features = { "src/test/resource/features/practice.feature" },
 		// tags = "@Positive_UnFreezeNSDLMaker",
 		glue = { "apphook", "com/stepdef" }, dryRun = false, plugin = { "pretty",
 				"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:" }, monochrome = true)
-public class TestRunner  {
-	
-	public static void main(String[] args) {
-        System.out.println("Inside Test Runner class");
-    }
+public class TestRunner {
+
+	@Test
+	public void sampleTest() {
+		System.out.println("Test executed!");
+	}
 }
