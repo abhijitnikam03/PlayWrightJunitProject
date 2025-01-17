a83e08b8b7609e7fca5c2adbf1c909712dcf4754
abhij <abhij@Abhijit.lan>
2025-01-17 12:35:11 +0530

    commit

--- src/test/java/apphook/AppHooks.java
+++ src/test/java/apphook/AppHooks.java
@@ -20,24 +20,45 @@ public class AppHooks {
 	protected Properties prop;
 	PracticePage practPage;
 	private ConfigReader configReader;
-	
-	@Before(order=0)
+
+	@Before(order = 0)
 	public void setup() throws IOException {
 		configReader = new ConfigReader();
 		prop = configReader.init_prop();
 		String browsername = prop.getProperty("browser").trim();
 		System.out.println("Property name is" + prop);
 		File file = new File("recordings/");
-		df= new DriverFactory();
-		df.initBrowser(browsername);	
+		df = new DriverFactory();
+		df.initBrowser(browsername);
 		practPage = new PracticePage(page);
+		String folderPath = "./reports";
+		File folder = new File(folderPath);
+		if (deleteDirectory(folder)) {
+			System.out.println("Folder deleted successfully.");
+		} else {
+			System.out.println("Failed to delete the folder.");
+		}
 
 	}
 
-	@After(order=0)
+	@After(order = 0)
 	public void Teardown(Scenario scenario) {
-		page=DriverFactory.getpage();
+		page = DriverFactory.getpage();
 		page.close();
 
+	}
+	
+	private static boolean deleteDirectory(File directory) {
+		if (directory.isDirectory()) {
+			File[] files = directory.listFiles();
+			if (files != null) { // Ensure it is not null
+				for (File file : files) {
+					deleteDirectory(file); // Recursive call for each file/directory
+				}
+			} else {
+				System.out.println("File not deleted");
+			}
+		}
+		return directory.delete(); // Delete the directory or file
 	}
 }
